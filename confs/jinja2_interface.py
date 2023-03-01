43a157ab8933c49aed284694c65ae59e76b414cb
HenryWinterbottom-NOAA <henry.winterbottom@noaa.gov>
2023-03-01 13:23:30 -0700

    Debugging.

--- confs/jinja2_interface.py
+++ confs/jinja2_interface.py
@@ -322,7 +322,7 @@ def _get_template_vars(tmpl_path: str) -> List:
     tmpl = _get_template(tmpl_path=tmpl_path)
 
     # Collect the templated variable names.
-    print(list(env.parse(tmpl)))
+    print(dir(env))
     quit()
 
     variables = list(meta.find_undeclared_variables(env.parse(tmpl)))
