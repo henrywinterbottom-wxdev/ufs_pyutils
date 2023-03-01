0d1e5a6fe6e27e8bb0c62fc326a3f7d96abd57bb
HenryWinterbottom-NOAA <henry.winterbottom@noaa.gov>
2023-03-01 14:05:50 -0700

    Debugging.

--- confs/jinja2_interface.py
+++ confs/jinja2_interface.py
@@ -165,8 +165,9 @@ def _fail_missing_vars(tmpl_path: str, in_dict: Dict) -> None:
 
         for item in data:
             if ("{{" and "}}" in item) and ("or" not in item):
-                string = re.search("{{ .* }}", item)
-                print(string.groups())
+                #string = re.search("{{ .* }}", item)
+                string = item[item.find("{{") + len(2):item.rfind("}}")]
+                print(string)
 
     # print(variables)
     quit()
