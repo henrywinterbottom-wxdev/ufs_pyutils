e1d38178f3bfd26bab047931f17083a8b8684707
HenryWinterbottom-NOAA <henry.winterbottom@noaa.gov>
2023-03-24 15:14:25 -0600

    Debugging.

--- ioapps/xml_interface.py
+++ ioapps/xml_interface.py
@@ -105,6 +105,12 @@ def read_xml(xml_path: str, remove_comments: bool = False,
 
     xml_dict = xmltodict.parse(xml_str)
 
+    for (item, value) in {"__ENTITY__": "&"}.items():
+        text = text.replace(item value)
+        print(text)
+
+    quit()
+
     print(xml_dict)
     quit()
 
