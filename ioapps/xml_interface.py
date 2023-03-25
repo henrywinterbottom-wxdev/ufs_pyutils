bb9656805322704d48e1110d4e19982e66527865
HenryWinterbottom-NOAA <henry.winterbottom@noaa.gov>
2023-03-25 17:39:00 -0600

    New functions.

--- ioapps/xml_interface.py
+++ ioapps/xml_interface.py
@@ -268,7 +268,7 @@ def write_xml_str(xml_dict: Dict, indent: int = 5) -> str:
 
 #    dtdfile = "/ufs_engines/rocoto/tools/rocoto_tools/DTD.dtd"
 #
-    tree = etree.fromstring(xml_str.encode(), doctype=doc_str)
+    tree = etree.fromstring(xml_str.encode(), base_url=doc_str)
 
     print(tree)
     quit()
