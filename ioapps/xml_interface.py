1879e4845f85c5a677b84f80147275e69910120f
HenryWinterbottom-NOAA <henry.winterbottom@noaa.gov>
2023-03-24 18:59:30 -0600

    Debugging.

--- ioapps/xml_interface.py
+++ ioapps/xml_interface.py
@@ -208,10 +208,9 @@ def read_xml(xml_path: str, remove_comments: bool = False) -> Dict:
         for (key, value) in XML_CHAR_DICT.items():
             xml_str_out = xml_str_in.replace(key, value)
 
-        parser = etree.XMLParser(resolve_entities=False)
-        root = etree.fromstring(bytes(xml_str_out), parser)
+        xml_dict = yaml.load(xml_str_out)
 
-        print(root)
+        print(xml_dict)
         quit()
 
         print(xml_dict)
