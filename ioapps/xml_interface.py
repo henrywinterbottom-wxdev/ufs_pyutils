dcec2294c85703c963cf213788ff664c2c17aa81
HenryWinterbottom-NOAA <henry.winterbottom@noaa.gov>
2023-03-25 17:23:41 -0600

    New functions.

--- ioapps/xml_interface.py
+++ ioapps/xml_interface.py
@@ -252,12 +252,8 @@ def write_xml_str(xml_dict: Dict, indent: int = 5) -> str:
 
     """
 
-    xml_str = "<!DOCTYPE workflow SYSTEM '/ufs_engines/rocoto/tools/rocoto_tools/DTD.dtd'>"
-    xml_str = xml_str + xmltodict.unparse(xml_dict)
-
-    print(xml_str)
-    quit()
-
+    # xml_str = "<!DOCTYPE workflow SYSTEM '/ufs_engines/rocoto/tools/rocoto_tools/DTD.dtd'>"
+    xml_str = xmltodict.unparse(xml_dict)
     xml_str = xml_str + \
         minidom.parseString(xml_str).toprettyxml(indent=indent*" ")
 
