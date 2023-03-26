b2a4438fe7eb487734adc8da70c8c3578141c594
HenryWinterbottom-NOAA <henry.winterbottom@noaa.gov>
2023-03-25 19:26:51 -0600

    Debugging resolver.

--- ioapps/xml_interface.py
+++ ioapps/xml_interface.py
@@ -294,7 +294,7 @@ def write_xml_str(xml_dict: Dict, indent: int = 5) -> str:
 
     xml_str = BytesIO(bs.encode_contents())
 
-    parser = etree.XMLParser(load_dtd=True, resolve_entities=True)
+    parser = etree.XMLParser(load_dtd=True, resolve_entities=False)
     # parser.resolvers.add(DTDResolver())
 
     # xml = '<!DOCTYPE doc SYSTEM "DTD.dtd"><doc>&myentity;</doc>'
