74bfff884c681f0ca56c1a99188a0a6e15276354
HenryWinterbottom-NOAA <henry.winterbottom@noaa.gov>
2023-03-25 19:42:35 -0600

    Debugging resolver.

--- ioapps/xml_interface.py
+++ ioapps/xml_interface.py
@@ -298,7 +298,7 @@ def write_xml_str(xml_dict: Dict, indent: int = 5) -> str:
 
         xml_str = xml_str.replace(f"{key}", f"{value}")  # IS THIS NEEDED?
 
-    linenum = find_line(string="xml_version", fp=xml_str.split())
+    linenum = find_line(string="xml_version", fp=xml_str.split("\n"))
     print(linenum)
     quit()
 
