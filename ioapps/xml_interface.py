de5073831086a8b97878ac14e13334d0275c697b
HenryWinterbottom-NOAA <henry.winterbottom@noaa.gov>
2023-03-25 19:28:08 -0600

    Debugging resolver.

--- ioapps/xml_interface.py
+++ ioapps/xml_interface.py
@@ -280,6 +280,8 @@ def write_xml_str(xml_dict: Dict, indent: int = 5) -> str:
 
     xml_str = xmltodict.unparse(xml_dict)
 
+    print(xml_str)
+
     xml_str = minidom.parseString(xml_str).toprettyxml(indent=indent*" ")
 
     for (key, value) in XML_SSYMS_DICT.items():
