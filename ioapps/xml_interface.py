b8eba1348f537d1f20bed16507f25fd64fe48b84
HenryWinterbottom-NOAA <henry.winterbottom@noaa.gov>
2023-03-25 18:46:41 -0600

    Debugging resolver.

--- ioapps/xml_interface.py
+++ ioapps/xml_interface.py
@@ -291,6 +291,8 @@ def write_xml_str(xml_dict: Dict, indent: int = 5) -> str:
 
     # xml = '<!DOCTYPE doc SYSTEM "DTD.dtd"><doc>&myentity;</doc>'
 
+    xml_str = '<!DOCTYPE workflow SYSTEM "DTD.dtd">' + xml_str
+
     tree = etree.parse(StringIO(xml_str), parser)
     root = tree.getroot()
     print(root.text)
