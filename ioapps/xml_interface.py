613c8a4cf2f972d6c69ff93330ae93efda2911ed
HenryWinterbottom-NOAA <henry.winterbottom@noaa.gov>
2023-03-25 20:06:10 -0600

    Debugging resolver.

--- ioapps/xml_interface.py
+++ ioapps/xml_interface.py
@@ -316,6 +316,9 @@ def write_xml_str(xml_dict: Dict, indent: int = 5) -> str:
 
     # tree = etree.parse(BytesIO(xml_str), parser)
     tree = etree.XML(xml_str, parser=parser)
+    xml_str = etree.tostring(tree)
+    print(xml_str)
+    quit()
     # print(etree.tostring(tree))
     # quit()
 
