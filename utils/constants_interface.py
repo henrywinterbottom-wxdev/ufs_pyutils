7f7d23c88e671142bd44b403520fbaa8e2e640b9
HenryWinterbottom-NOAA <henry.winterbottom@noaa.gov>
2023-03-31 06:46:15 -0600

    Updates.

--- utils/constants_interface.py
+++ utils/constants_interface.py
@@ -65,13 +65,13 @@ History
 
 # ----
 
-from astropy import units
-
-# ----
-
 __author__ = "Henry R. Winterbottom"
 __maintainer__ = "Henry R. Winterbottom"
 __email__ = "henry.winterbottom@noaa.gov"
+
+# ----
+
+from astropy import units
 
 # ----
 
