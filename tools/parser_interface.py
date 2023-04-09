6792059ae52f1be50241047ccbead623d784627a
HenryWinterbottom-NOAA <henry.winterbottom@noaa.gov>
2023-04-09 14:36:24 -0600

    Added list comprehension exception handler.

--- tools/parser_interface.py
+++ tools/parser_interface.py
@@ -763,7 +763,7 @@ def find_commonprefix(strings_list: List) -> str:
 # ----
 
 
-def handler(func: Callable, handle: lambda errmsg: errmsg,
+def handler(func: Callable, handle=lambda err: err,
             return_none: bool = False, raise_exeception: bool = False,
             *args, **kwargs):
     """
