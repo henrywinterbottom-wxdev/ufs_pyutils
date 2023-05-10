2294cd4b2a908e2b5e1bc4956fa2332b92140c7e
HenryWinterbottom-NOAA <henry.winterbottom@noaa.gov>
2023-05-09 18:43:07 -0600

    Added new functions.

--- utils/schema_interface.py
+++ utils/schema_interface.py
@@ -37,35 +37,41 @@ Functions
         This function builds a Python schema dictionary using the And
         attribute.
 
-    build_schema(yaml_path)
+    __buildtbl__(cls_schema, cls_opts, logger_method)
+
+        This function compiles and writes a table using the defined
+        logger method for the respective schema attributes and the
+        values corresponding to the respective application.
+
+    __def_schema__(schema_dict, ignore_extra_keys=True)
+
+        This function defines the schema object in accordance with the
+        specified parameters.
+
+    build_schema(schema_def_dict)
 
         This function builds a schema provided a YAML-formatted file
         containing the variable types and attributes (if necessary);
-        supported schema types are mandatory, optional (Optional), and
-        multi-type (Or) variables.
+        supported schema types are mandatory (e.g., `required = True`)
+        and optional (e.g., `required = False` or is not defined
+        within the schema definitions (`schema_def_dict` key and value
+        pairs.
 
         The YAML-formatted file containing the schema attributes
         should be formatted similar to the example below.
 
-        required:
-            variable1: bool
-            variable2: float
-            variable3: int
+        variable1:
+            required: False
+            type: bool
+            default: True
 
-        optional:
-            variable4:
-                type: bool
-                default: False
-            variable5:
-                type: bool
-                default: True
-            variable6:
-                type: float
-                default: 1.0
+        variable2:
+            required: True
+            type: float
 
-        multitype:
-            variable7: str, int
-            variable8: float, bool, str
+        variable3:
+            type: int
+            default: 1
 
     check_opts(key, valid_opts, data, check_and=False)
 
@@ -78,10 +84,21 @@ Functions
         respective schema is not validated an exception will be
         raised; otherwise this function is passive.
 
+    validate_schema(cls_schema, cls_opts, ignore_extra_keys=True,
+                    write_table=True, logger_method="info")
+
+        This method validates the specified caller method options
+        against the specified schema; schema optional values (denoted
+        as `Optional` instances) are assigned default values during
+        instances when the caller method options (`cls_opts`) as not
+        defined a corresponding value.
+
 Requirements
 ------------
 
 - schema; https://github.com/keleshev/schema
+
+- tabulate; https://github.com/gregbanks/python-tabulate
 
 Author(s)
 ---------
@@ -107,19 +124,25 @@ __email__ = "henry.winterbottom@noaa.gov"
 
 # ----
 
+from collections import OrderedDict
 from pydoc import locate
 from typing import Dict, List
 
-from confs.yaml_interface import YAML
-from schema import And, Optional, Or, Schema
+from schema import And, Optional, Schema
+from tabulate import tabulate
 from tools import parser_interface
 
 from utils.exceptions_interface import SchemaInterfaceError
+from utils.logger_interface import Logger
 
 # ----
 
 # Define all available attributes.
-__all__ = ["build_schema", "check_opts", "validate_opts"]
+__all__ = ["build_schema", "check_opts", "validate_opts", "validate_schema"]
+
+# ----
+
+logger = Logger(caller_name=__name__)
 
 # ----
 
@@ -161,142 +184,226 @@ def __andopts__(key: str, valid_opts: List) -> Dict:
 # ----
 
 
-def build_schema(yaml_path: str) -> Dict:
+def __buildtbl__(cls_schema: Dict, cls_opts: Dict, logger_method: str) -> None:
     """
     Description
     -----------
 
-    This function builds a schema provided a YAML-formatted file
-    containing the variable types and attributes (if necessary);
-    supported schema types are mandatory, optional (Optional), and
-    multi-type (Or) variables.
-
-    The YAML-formatted file containing the schema attributes should be
-    formatted similar to the example below.
-
-    required:
-        variable1: bool
-        variable2: float
-        variable3: int
-
-    optional:
-        variable4:
-            type: bool
-            default: False
-        variable5:
-            type: bool
-            default: True
-        variable6:
-            type: float
-            default: 1.0
-
-    multitype:
-        variable7: str, int
-        variable8: float, bool, str
+    This function compiles and writes a table using the defined logger
+    method for the respective schema attributes and the values
+    corresponding to the respective application.
 
     Parameters
     ----------
 
-    yaml_path: str
+    cls_schema: Dict
 
-        A Python string specifying the YAML-formatted file containing
-        the schema attributes.
+        A Python dictionary containing the calling class schema.
 
-    Returns
-    -------
+    cls_opts: Dict
 
-    schema_dict: Dict
+        A Python dictionary containing the options (i.e., parameter
+        arguments, keyword arguments, etc.,) passed to the respective
+        calling class.
 
-        A Python dictionary containing the define schema.
+    logger_method: str
+
+        A Python string specifying the logger method to be usedf to
+        write the schema attributes table.
 
     Raises
     ------
 
     SchemaInterfaceError:
 
-        - raised if the a optional variable value is not defined
-          properly within YAML-formatted file path.
-
-        - raised if the type-class for the multitype schema variable
-          instances is not formatted correctly (i.e., is not a
-          string).
-
-        - raised if a multitype schema variable instance does not
-          contain two or more attributes within the YAML-formatted
-          file.
+        - raised if the logger method defined by `logger_method` upon
+          entry is not supported.
 
     """
 
-    # Read the YAML-formatted file containing the schema.
-    yaml_dict = YAML().read_yaml(yaml_file=yaml_path)
+    # Build the table attributes.
+    header = ["Variable", "Type", "Optional", "Value"]
+    table = []
+    for (cls_key, _) in OrderedDict(cls_schema).items():
+        if "bool" in cls_schema[cls_key].__name__:
+            value = str(bool(cls_opts[cls_key]))
+        else:
+            value = cls_opts[cls_key.key]
 
-    # Define any mandatory (e.g., required) schema attributes; proceed
-    # accordingly.
+        if isinstance(cls_key, Optional):
+            msg = [cls_key.key, cls_schema[cls_key].__name__, "True", value]
+
+        if not isinstance(cls_key, Optional):
+            msg = [cls_key, cls_schema[cls_key].__name__, "False", value]
+        table.append(msg)
+
+    # Write the table using the logger method.
+    msg = (
+        "\n\n"
+        + tabulate(
+            table,
+            header,
+            tablefmt="outline",
+            numalign=("center", "center", "center", "center"),
+            colalign=("center", "center", "center", "center"),
+            disable_numparse=True,
+        )
+        + "\n\n"
+    )
+
+    logmethod = parser_interface.object_getattr(
+        object_in=logger, key=logger_method.lower(), force=True
+    )
+    if logmethod is None:
+        msg = f"Logger method {logger_method} is not supported. Aborting!!!"
+        raise SchemaInterfaceError(msg=msg)
+    logmethod(msg=msg)
+
+
+# ----
+
+
+def __def_schema__(schema_dict: Dict, ignore_extra_keys: bool = True) -> Schema:
+    """
+    Description
+    -----------
+
+    This function defines the schema object in accordance with the
+    specified parameters.
+
+    Parameters
+    ----------
+
+    schema_dict: Dict
+
+        A Python dictionary containing the defined schema.
+
+    Keywords
+    --------
+
+    ignore_extra_keys: bool, optional
+
+        A Python boolean valued variable specifying whether to ignore
+        extra keys that are contained with the Python dictionary
+        containing the schema to be validated.
+
+    Returns
+    -------
+
+    schema: Schema
+
+        A Python object containing the defined schema object.
+
+    """
+
+    # Define the schema object.
+    schema = Schema([schema_dict], ignore_extra_keys=ignore_extra_keys)
+
+    return schema
+
+
+# ----
+
+
+def build_schema(schema_def_dict: Dict) -> Dict:
+    """
+    Description
+    -----------
+
+    This function builds a schema provided a YAML-formatted file
+    containing the variable types and attributes (if necessary);
+    supported schema types are mandatory (e.g., `required = True`) and
+    optional (e.g., `required = False` or is not defined within the
+    schema definitions (`schema_def_dict` key and value pairs.
+
+    The YAML-formatted file containing the schema attributes should be
+    formatted similar to the example below.
+
+    variable1:
+        required: False
+        type: bool
+        default: True
+
+    variable2:
+        required: True
+        type: float
+
+    variable3:
+        type: int
+        default: 1
+
+    Parameters
+    ----------
+
+    schema_def_dict: Dict
+
+        A Python dictionary containing the schema definition
+        attributes; these contents are collect from a YAML-formatted
+        file containing the respective variables and corresponding
+        attributes and defined above.
+
+    Returns
+    -------
+
+    schema_dict: Dict
+
+        A Python dictionary containing the defined schema.
+
+    Raises
+    ------
+
+    SchemaInterfaceError:
+
+        - raised if an exception is encountered while defining the
+          default value for optional schema attributes; this is most
+          often encountered when a key and value pair corresponding to
+          the attribute `default` for an optional variable is not
+          defined for the respective schema variable.
+
+    """
+
+    # Build the schema for the respective application.
     schema_dict = {}
+    for schema_def in schema_def_dict:
+        schema_attr_dict = parser_interface.dict_key_value(
+            dict_in=schema_def_dict, key=schema_def, no_split=True
+        )
 
-    required_dict = parser_interface.dict_key_value(
-        dict_in=yaml_dict, key="required", force=True
-    )
-    if required_dict is not None:
-        for (required_key, required_value) in required_dict.items():
-            schema_dict[required_key] = locate(required_value)
+        # Check whether the schema attribute is optional (e.g.,
+        # required); proceed accordingly.
+        if "required" in schema_attr_dict:
+            schema_attr_dict["required"] = schema_def_dict[schema_def]["required"]
+            if schema_attr_dict["required"]:
+                schema_dict[schema_def] = locate(schema_attr_dict["type"])
 
-    # Define any optional schema attributes; proceed accordingly.
-    optional_dict = parser_interface.dict_key_value(
-        dict_in=yaml_dict, key="optional", force=True
-    )
-    if optional_dict is not None:
-        for varname in optional_dict:
+        else:
+            schema_attr_dict["required"] = False
 
-            varname_dict = parser_interface.dict_key_value(
-                dict_in=optional_dict, key=varname, force=True
-            )
-            if varname_dict is None:
+        if not schema_attr_dict["required"]:
+            try:
                 msg = (
-                    f"The schema entry for optional variable {varname} in "
-                    f"YAML-formatted file path {yaml_path} is not defined "
-                    f"correctly; expected Dict but received NoneType. "
-                    "Aborting!!!"
+                    f"Found optional variable {schema_def}; setting to type "
+                    f"{schema_attr_dict['type']}; default value is "
+                    f"{schema_attr_dict['default']}."
                 )
-                raise SchemaInterfaceError(msg=msg)
-
-            varname_obj = parser_interface.dict_toobject(in_dict=varname_dict)
-            schema_dict[Optional(varname, default=varname_obj.default)] = locate(
-                varname_obj.type
-            )
-
-    # Define any schema attributes permitted to have multiple
-    # supported Python data types; proceed accordingly.
-    multitype_dict = parser_interface.dict_key_value(
-        dict_in=yaml_dict, key="multitype", force=True
-    )
-    if multitype_dict is not None:
-        for varname in multitype_dict:
-
-            if not isinstance(multitype_dict[varname], str):
+                logger.warn(msg=msg)
+                schema_dict[
+                    Optional(schema_def, default=schema_attr_dict["default"])
+                ] = locate(schema_attr_dict["type"])
+            except Exception as errmsg:
                 msg = (
-                    f"Multiple type variable {varname} in YAML-formatted file path "
-                    f"{yaml_path} is not valid; expected str but received "
-                    f"{type(multitype_dict[varname])}. Aborting!!!"
+                    f"Defining schema for variable {schema_def} failed with "
+                    f"error {errmsg}. Aborting!!!"
                 )
-                raise SchemaInterfaceError(msg=msg)
+                raise SchemaInterfaceError(msg=msg) from errmsg
 
-            if len(multitype_dict[varname]) <= 1:
-                msg = (
-                    "Multi-type variables must contain 2 or more supported Python types; "
-                    f"for variable {varname} in YAML-formatted file path {yaml_path} "
-                    f"received {multitype_dict[varname]}. Aborting!!!"
-                )
-                raise SchemaInterfaceError(msg=msg)
-
-            type_gen = (
-                locate(vartype.strip())
-                for vartype in multitype_dict[varname].split(",")
+        if schema_attr_dict["required"]:
+            msg = (
+                f"Found required variable {schema_def}; setting to type "
+                f"{schema_attr_dict['type']}."
             )
-
-            schema_dict[varname] = Or(
-                *(next(type_gen) for vartype in multitype_dict[varname].split(","))
-            )
+            logger.info(msg=msg)
+            schema_dict[schema_def] = locate(schema_attr_dict["type"])
 
     return schema_dict
 
@@ -370,7 +477,9 @@ def check_opts(key: str, valid_opts: List, data: Dict, check_and: bool = False) 
 # ----
 
 
-def validate_opts(cls_schema: Dict, cls_opts: Dict) -> None:
+def validate_opts(
+    cls_schema: Dict, cls_opts: Dict, ignore_extra_keys: bool = True
+) -> None:
     """
     Description
     -----------
@@ -392,6 +501,15 @@ def validate_opts(cls_schema: Dict, cls_opts: Dict) -> None:
         arguments, keyword arguments, etc.,) passed to the respective
         calling class.
 
+    Keywords
+    --------
+
+    ignore_extra_keys: bool, optional
+
+        A Python boolean valued variable specifying whether to ignore
+        extra keys that are contained with the Python dictionary
+        containing the schema to be validated (`cls_opts`).
+
     Raises
     ------
 
@@ -403,15 +521,109 @@ def validate_opts(cls_schema: Dict, cls_opts: Dict) -> None:
     """
 
     # Define the schema.
-    schema = Schema([cls_schema])
+    schema = __def_schema__(schema_dict=cls_schema,
+                            ignore_extra_keys=ignore_extra_keys)
 
     # Check that the class attributes are valid; proceed accordingly.
     try:
 
         # Validate the schema.
-        schema.validate([cls_opts], ignore_extra_keys=True)
+        schema.validate([cls_opts])
 
     except Exception as errmsg:
 
         msg = f"Schema validation failed with error {errmsg}. Aborting!!!"
         raise SchemaInterfaceError(msg=msg) from errmsg
+
+
+# ----
+
+
+def validate_schema(
+    cls_schema: Dict,
+    cls_opts: Dict,
+    ignore_extra_keys: bool = True,
+    write_table: bool = True,
+    logger_method: str = "info",
+) -> Dict:
+    """
+    Description
+    -----------
+
+    This method validates the specified caller method options against
+    the specified schema; schema optional values (denoted as
+    `Optional` instances) are assigned default values during instances
+    when the caller method options (`cls_opts`) as not defined a
+    corresponding value.
+
+    Parameters
+    ----------
+
+    cls_schema: Dict
+
+        A Python dictionary containing the calling class schema.
+
+    cls_opts: Dict
+
+        A Python dictionary containing the options (i.e., parameter
+        arguments, keyword arguments, etc.,) passed to the respective
+        calling class.
+
+    Keywords
+    --------
+
+    ignore_extra_keys: bool, optional
+
+        A Python boolean valued variable specifying whether to ignore
+        extra keys that are contained with the Python dictionary
+        containing the schema to be validated (`cls_opts`).
+
+    write_table: bool, optional
+
+        A Python boolean valued variable specifying whether to write
+        the schema attributes table using the specified logger method.
+
+    logger_method: str, optional
+
+        A Python string specifying the logger method to be usedf to
+        write the schema attributes table.
+
+    Returns
+    -------
+
+    cls_opts: Dict
+
+        A Python dictionary containing the options defined upon entry
+        and updated to contain any optional schema default key and
+        value pairs if not specified within the Python dictionary upon
+        entry.
+
+    """
+
+    # Define the schema.
+    schema = __def_schema__(schema_dict=cls_schema,
+                            ignore_extra_keys=ignore_extra_keys)
+
+    # Check that any optional schema attributes have been specified by
+    # the calling class attributes (`cls_opts`); if not, assign the
+    # schema default key and value pairs; proceed accordingly.
+    for (cls_key, _) in cls_schema.items():
+        if isinstance(cls_key, Optional):
+            if cls_key.key not in cls_opts:
+                msg = (
+                    f"Schema optional value {cls_key.key} has not be defined; setting to "
+                    f"default value {cls_key.default}."
+                )
+                logger.warn(msg=msg)
+
+                cls_opts[cls_key.key] = cls_key.default
+
+    # Validate the schema and build and write a table containing the
+    # calling class attributes; proceed accordingly.
+    schema.validate([cls_opts])
+    if write_table:
+        __buildtbl__(
+            cls_schema=cls_schema, cls_opts=cls_opts, logger_method=logger_method
+        )
+
+    return cls_opts
