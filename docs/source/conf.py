653e76d538a29a2ba77e83caae929f134f91b503
HenryWinterbottom-NOAA <henry.winterbottom@noaa.gov>
2023-08-29 18:38:47 -0600

    Debugging API documenter.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,8 +12,14 @@
 #
 import os
 import sys
+import pip
 
 sys.path.insert(0, os.path.abspath('.'))
+pip.main(["install", "furo==2023.03.27"])
+pip.main(["install", "sphinx-autoapi"])
+pip.main(["install", "sphinx-autodoc-typehints"])
+pip.main(["install", "git+https://www.github.com/HenryWinterbottom-NOAA/numpydoc@feature/ufs_pyutils_updates"])
+
 
 # -- Project information -----------------------------------------------------
 
